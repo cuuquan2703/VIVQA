4cfd32447518f55b31845dc1c2e9884e5321098e
Nguyễn Đức Anh <ngducanh1611@gmail.com>
2022-06-02 09:58:36 +0700

    update build backbone

--- base_model.py
+++ base_model.py
@@ -613,7 +613,7 @@ def build_GuidedAtt(args):
 
     print(f'Loading CNN ({args.cnn_image_pretrained}) feature extractor...')
     v_cnn_dim = args.v_cnn_dim
-    v_cnn_emb = initialize_backbone_model(args.cnn_backbone, use_imagenet_pretrained=args.cnn_image_pretrained)[0]
+    v_cnn_emb = initialize_backbone_model(args.cnn_image_pretrained, use_imagenet_pretrained=True)[0]
 
     visual_vit_guided_att = GuidedTransformerEncoder(v_vit_dim, q_dim, args.num_heads, args.hidden_dim, args.dropout)
     visual_cnn_guided_att = GuidedTransformerEncoder(v_cnn_dim, q_dim, args.num_heads, args.hidden_dim, args.dropout)
