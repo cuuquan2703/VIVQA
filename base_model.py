d21c70612d2ce8547a2f89c193ec1636c1e0fc3a
1412010 <ngducanh1611@gmail.com>
2022-06-28 13:17:40 +0700

    [NEW] try ViT only

--- base_model.py
+++ base_model.py
@@ -652,9 +652,9 @@ def build_GuidedAtt(args):
 
     return GuidedAttentionModel(
         q_emb, 
-        [v_cnn_emb], 
-        [visual_cnn_guided_att],
-        [visual_cnn_reduced],
+        [v_vit_emb], 
+        [visual_vit_guided_att],
+        [visual_vit_reduced],
         fusion,
         question_guided_att,
         classifier,
