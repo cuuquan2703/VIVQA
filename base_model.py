064907acf12d4fe319ff6c8c731980591b4ff226
1412010 <ngducanh1611@gmail.com>
2022-06-29 23:50:38 +0700

    fix visual model order

--- base_model.py
+++ base_model.py
@@ -652,9 +652,9 @@ def build_GuidedAtt(args):
 
     return GuidedAttentionModel(
         q_emb, 
-        [v_cnn_emb, v_vit_emb], 
-        [visual_cnn_guided_att, visual_vit_guided_att],
-        [visual_cnn_reduced, visual_vit_reduced],
+        [v_vit_emb, v_cnn_emb], 
+        [visual_vit_guided_att, visual_cnn_guided_att],
+        [visual_vit_reduced, visual_cnn_reduced],
         fusion,
         question_guided_att,
         classifier,
